4b20656194c7b77ae53b66e0dffac37cb7d18f75
marylynokotta <marylynokotta@gmail.com>
2026-05-27 14:28:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+ const counters = document.querySelectorAll('.counter');
+
+    counters.forEach(counter => {
+
+      counter.innerText = '0';
+
+      const updateCounter = () => {
+
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+
+        const increment = target / 200;
+
+        if(current < target){
+
+          counter.innerText = `${Math.ceil(current + increment)}`;
+
+          setTimeout(updateCounter, 10);
+
+        } else {
+
+          counter.innerText = target;
+
+        }
+
+      };
+
+      updateCounter();
+
+    });
+
+
+    document.querySelector('.primary-btn')
+      .addEventListener('click', () => {
+
+        alert('🎥 Highlights Coming Soon!');
+
+      });
